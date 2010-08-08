830d0db4a43d1706566e8d5404dfce9df9e9c6e1
Joachim Bengtsson <joachimb@gmail.com>
2010-08-08 10:33:25 +0200

    differentiate between 'interactive' commands (prints to stdout) and 'capturing' commands (returns its value as array of lines). default to interactive, as that's the Unix default. Run command as "capture" by suffixing "_". Example:
    	10:35:04 nevyn:ocsh$ ls
    	ocsh.rb				powershell for realz.rtf
    	=> true
    	10:35:06 nevyn:ocsh$ ls_
    	=> ["ocsh.rb", "powershell for realz.rtf"]

--- ocsh.rb
+++ ocsh.rb
@@ -1,18 +1,42 @@
 #!/usr/bin/env ruby
 require 'irb'
 require 'pp'
+require 'stringio'
 
-def method_missing(*stuff)
-  return Kernel::method_missing(*stuff) if system("which #{stuff[0]} > /dev/null") != true
-  return `#{stuff.join " "}`.split "\n"
+def method_missing(cmd, *argv)
+  print_to_stdout = true
+  if(cmd.to_s[-1] == "_"[0])
+    cmd = cmd.to_s[0...-1].intern
+    print_to_stdout = false
+  end
+  which = `which #{cmd}`.strip
+  return Kernel::method_missing(cmd) if $? != 0
+  if print_to_stdout
+    system("#{cmd} #{argv.join(" ")}")
+  else
+    `#{cmd} #{argv.join(" ")}`.split("\n")
+  end
+#  lines = []
+#  IO.popen("#{cmd} #{argv.join(" ")}") do |pipe|
+#    buf = pipe.read
+#    print buf if print_to_stdout
+#    parts = buf.split("\n")
+#    if lines.length == 0
+#      lines = parts 
+#    else
+#      lines[-1] += parts[0]
+#      lines += parts[1..-1]
+#    end
+#  end
+#  lines
 end
 
 module IRB
   class Irb
     def prompt(prompt, ltype, indent, line_no)
       time = Time.new.strftime("%H:%M:%S")
-      user = whoami[0]
-      wd = basename Dir.getwd
+      user = whoami_[0]
+      wd = basename_ Dir.getwd
       wd = "~" if Dir.getwd == ENV["HOME"]
       
       "\033[46m#{time} #{user}:#{wd}#{ltype and ltype or "$"}\033[0m " + " "*indent
